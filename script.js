8f63a986f2bb479b9d89252da1ccce09e76c6530
Sergei-rgb@ <sergei25102007@bk.ru>
2024-09-10 17:58:11 +0400

    main

--- script.js
+++ script.js
@@ -13,17 +13,41 @@ var view = {
     }
 };
 
-// view.displayMiss("A0");
-// view.displayHit("34");
-// view.displayMiss("F5");
-// view.displayHit("B2");
-// view.displayMiss("C5");
-// view.displayHit("C6");
+view.displayMiss("00");
+view.displayHit("34");
+view.displayMiss("55");
+view.displayHit("12");
+view.displayMiss("25");
+view.displayHit("26");
 
 
 view.displayMessage("Тук-тук, это работает?");
 
 
-var ship1 = { location: ["A0", "", "30"], hits: ["hits", "hits", "hits"] };
-var ship2 = { location:  ["32", "33", "34"], hits:  ["hits", "hits", "hits"] };
-var ship3 = { location: ["63", "64", "65",], hits: ["hits", "hits", "hits"] };
+var ship1 = { location: ["10", "20", "30"], hits: ["", "", ""] };
+var ship2 = { location:  ["32", "33", "34"], hits:  ["", "", ""] };
+var ship3 = { location: ["63", "64", "65",], hits: ["", "", "hit"] };
+
+var ships = [{ location: ["10", "20", "30"], hits: ["", "", ""] },
+            { location: ["32", "33", "34"], hits: ["", "", ""] },
+            { location: ["63", "64", "65"], hits: ["", "", "hit"]  }];
+
+
+
+var ships = [{ location: ["06", "16", "26"], hits: ["hit", "", ""] },
+            { location: ["24", "34", "44"], hits: ["", "", ""] },
+            { location: ["10", "11", "12"], hits: ["", "", ""]  }];
+
+var ships = [{ location: ["31", "41", "51"], hits: ["", "", ""] },
+            { location: ["14", "24", "34"], hits: ["", "hit", ""] },
+            { location: ["00", "01", "02"], hits: ["", "", ""]  }];
+
+var ship2 = ships [1];
+var location = ship2.location;
+console.log("Location is " + location[ship1])
+
+var ship3 = ships[2];
+var hits = ship3. hits;
+if ("ship3" === "hit") {
+    console.log("Ouch, hit on third ship at location one");
+}
