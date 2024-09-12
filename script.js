8525eaf92f3acc64a8e66e1fd06f2d165b8e55be
Sergei-rgb@ <sergei25102007@bk.ru>
2024-09-12 17:07:59 +0400

    main

--- script.js
+++ script.js
@@ -22,9 +22,39 @@ view.displayHit("26");
 
 view.displayMessage("Tap tap, is this thing on?")
 
-var ship1 = { location: ["10", "20", "30"], hits: ["", "", ""] };
-var ship2 = { location: ["32", "33", "34"], hits: ["", "", ""] };
-var ship3 = { location: ["63", "64", "65"], hits: ["", "", "hit"] };
+var ship1 = { locations: ["10", "20", "30"], hits: ["", "", ""] };
+var ship2 = { locations: ["32", "33", "34"], hits: ["", "", ""] };
+var ship3 = { locations: ["63", "64", "65"], hits: ["", "", "hit"] };
 
-var ships = [{ location: ["10", "20", "30"], hits: ["", "", ""] }, 
-}]
+var ships = [{ locations: ["10", "20", "30"], hits: ["", "", ""] },
+              { locations: ["32", "33", "34"], hits: ["", "", ""] },
+            { locations: ["63", "64", "65"], hits: ["", "", "hit"]} ];
+
+var ships = [{ locations: ["06", "16", "26"], hits: ["hit", "", ""] },
+         { locations: ["24", "34", "44"], hits: ["", "", ""] },
+          { locations: ["10", "11", "12"], hits: ["", "", ""]} ];
+
+
+    var model = {
+    boardSize: 7,
+    numShips: 3,
+    shipsSunk: 0,
+    shipLenght: 3,
+
+ships: [{ locations: ["06", "16", "26"], hits: ["", "", ""] },
+        { locations: ["24", "34", "44"], hits: ["", "", ""] },
+        { locations: ["10", "11", "12"], hits: ["", "", ""] }],
+
+fire: function(guess) {
+    for (var i = 0; i < this.numShips; i++) {
+        var ship = this.ships[i];
+        location = ships.locations
+        var index = locations.indexOf(guess);
+        if (index >= 0) {
+            ship.hits[index] = "hit";
+            return true;
+        }
+        return false;
+     }
+    }
+  };
