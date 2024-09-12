6c1c89133228b9003ce6318e731267e09f77d5e1
Sergei-rgb@ <sergei25102007@bk.ru>
2024-09-12 06:02:31 +0400

    main

--- script.js
+++ script.js
@@ -21,3 +21,10 @@ view.displayMiss("25");
 view.displayHit("26");
 
 view.displayMessage("Tap tap, is this thing on?")
+
+var ship1 = { location: ["10", "20", "30"], hits: ["", "", ""] };
+var ship2 = { location: ["32", "33", "34"], hits: ["", "", ""] };
+var ship3 = { location: ["63", "64", "65"], hits: ["", "", "hit"] };
+
+var ships = [{ location: ["10", "20", "30"], hits: ["", "", ""] }, 
+}]
